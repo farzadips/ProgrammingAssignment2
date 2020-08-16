0dc6766d31c336acfd2da1a665798cf296a23d2d
Farzad Imanpour <32436413+farzadips@users.noreply.github.com>
2020-08-16 20:23:48 +0430

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+setwd('C:/Users/farzadips/Desktop/R')
 
-## Write a short comment describing this function
+## set the solved value "s" as a nil
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  j <- NULL
+  set <- function(y){
+  x <<- y
+  j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  ## then trying to store in the cache by using model
+  list(set = set, get = get, 
+  setInverse = setInverse, 
+  getInverse = getInverse)
 }
 
+## Same here, changed "mean" and  "m"  to "solve" and"s"
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   ##first tries to see whether this data is available in cashe or not
+  j <- x$getInverse()
+  if(!is.null(j)){
+  message("getting cached data")
+  return(j)
+  }
+  ##if not do the calculation (find inverse)
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
 }
